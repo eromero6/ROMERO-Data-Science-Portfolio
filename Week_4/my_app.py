95ea77f45b9dc7dfc2e5eaf9c96788c686b2b79e
eromero6 <eromero6@nd.edu>
2026-02-04 12:51:41 -0500

    update

---
+++ Week_4/my_app.py
@@ -0,0 +1,30 @@
+# my_app.py
+
+import streamlit as st
+
+st.title("Hello, Streamlit")
+
+st.markdown("# Hello Streamlit!")
+
+st.write("This is my first Streamlit app!")
+
+### Loading our csv file
+import pandas as pd
+
+st.subheader("Exploring our Dataset")
+
+# Load the CSV file
+
+df = pd.read_csv("data/sample_data.csv")
+
+st.write("Here's our data!")
+st.dataframe(df)
+
+city = st.selectbox("Select a city", df["City"].unique())
+filtered_df = df[df["City"] == city]
+
+st.write(f"People in {city}")
+st.dataframe(filtered_df)
+
+# Add bar chart
+st.bar_chart(df["Salary"])
